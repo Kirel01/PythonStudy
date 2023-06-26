91a3f6b4494ca3938ff6f3a53cf7d4452213cd28
Kirel01 <122865179+Kirel01@users.noreply.github.com>
2023-06-26 09:22:43 +0900

    Python 0626

---
+++ 0626_DL1_1/dl1_perceptron.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Perceptron(object):
+    '''
+        매개변수
+            - eto : 학습률
+            - n_iter : 훈련 데이터셋 반복 횟수
+            - random_seed :
+    '''
+    def __init__(self, eta=0.01, n_iter=50, random_seed=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_seed = random_seed
+
+    '''
+        훈련 데이터 학습함수의 매개변수
+            - X : 훈련 데이터 [n_sample, n_features]
+                - n_sample개의 샘플과 n_features개의 특성으로 이루어진 훈련 데이터
+            - y : target 값 [n_sample]
+    '''
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_seed)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size= 1+X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X,y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update + xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+
+    def net_input(self, X):
+        '''입력 계산'''
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        '''클래스 레이블 반환(예측값 출력)'''
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
